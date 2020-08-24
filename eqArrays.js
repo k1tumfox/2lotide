67a44ab374065c35151e82827cca6b16a3b3ba9b
Tenyson Kwon <tenysonkwon@gmail.com>
2020-08-24 17:35:18 +0000

    refac eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,3 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
 
 const eqArrays = (a1, a2) => {
   
@@ -19,10 +16,4 @@ const eqArrays = (a1, a2) => {
   return true;
 };
 
-// eqArrays([1, 2, 3], [1, 2, 3]) // => true
-// eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+module.exports = eqArrays;
