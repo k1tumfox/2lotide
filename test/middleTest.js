80e16a48c4a62ea90555587d4aab119dc387aafa
Tenyson Kwon <tenysonkwon@gmail.com>
2020-08-24 18:42:28 +0000

    middle and tail with Mocha & chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,10 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
+describe("#middle", () => {
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+});
 
-assertArraysEqual(  middle([1, 2, 3, 4, 5]),   [3]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,27 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const {assert} = require('chai');
 
-//tests
-let ace = ["Hello", "Lighthouse", "Labs"];
-const result = tail(ace);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-console.log(ace);
+describe("", () => {
+  it("return ['Lighthouse', 'Labs']", () => {
+    let ace = ["Hello", "Lighthouse", "Labs"];
+    const result = tail(ace);
+    assert.strictEqual(result[0], "Lighthouse");
+    assert.strictEqual(result[1], "Labs");
+    assert.deepEqual(result, ['Lighthouse', 'Labs']);
+  });
 
-//edgecase1, 1 element array should return empty
-let spade = [6];
-const result1 = tail(spade);
-assertEqual(result1[0], undefined);
+  it("return empty when given 1 element array", () => {
+    let spade = [6];
+    const result = tail(spade);
+    assert.strictEqual(result[0], undefined);
+  });
 
-//edgecase2, emtpy array should return empty
-let club = [];
-const result2 = tail(club);
-assertEqual(result2[0], undefined);
+  it("return empty when given empty array", () => {
+    let club = [];
+    const result = tail(club);
+    assert.strictEqual(result[0], undefined);
+  });
+
+});
+
